87145bf3f9ee9148bb45fcff7387ffed850e7d40
serena-giacomelli <126363214+serena-giacomelli@users.noreply.github.com>
2024-05-25 12:24:28 -0300

    Add files via upload

---
+++ dist/usuarios.js
@@ -0,0 +1,10 @@
+export class Usuario {
+    constructor(nombre, apellido, dni, email, fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
+//# sourceMappingURL=usuarios.js.map
